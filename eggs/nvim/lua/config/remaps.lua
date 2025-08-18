3773b3d7db70186e9a74cc82e1fbe56067ac811f
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-08-18 12:09:29 -0300

    feat(nvim): change bind to open explorer

--- eggs/nvim/lua/config/remaps.lua
+++ eggs/nvim/lua/config/remaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- Moves the lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -14,14 +14,12 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- Copy and Paste
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
+vim.keymap.set({ "n", "v" }, "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>p", "\"+p")
 
 -- Deletion
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- Misc
 vim.keymap.set("n", "Q", "<nop>")
