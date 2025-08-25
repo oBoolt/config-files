423f1bd8a2ccb9f3168d1837ca80066a6f6530cc
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-08-25 17:43:33 -0300

    feat(eww): add 'powermenu' window

--- eggs/eww/scripts/toggle.sh
+++ eggs/eww/scripts/toggle.sh
@@ -2,7 +2,7 @@
 
 CACHE="${XDG_CACHE_HOME:-${HOME}/.cache}"
 EWW="eww --config $(eww get EWW_CONFIG_DIR)"
-WINDOWS=("tray")
+WINDOWS=("tray" "powermenu")
 
 toggle() {
     local LOCK_FILE="$CACHE/eww-$1.lock"
@@ -13,6 +13,9 @@ toggle() {
     else
         touch $LOCK_FILE
         $EWW close ${WINDOWS[*]}
+        for window in ${WINDOWS[@]}; do
+            $EWW update $window=false
+        done
         $EWW open $1
         $EWW update $1=true
     fi
