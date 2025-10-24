030027eeac59929eaaf5521d577593113f4620e7
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-10-24 15:15:16 -0300

    fix(nvim): update colorscheme after download

--- eggs/nvim/lua/config/opts.lua
+++ eggs/nvim/lua/config/opts.lua
@@ -1,6 +1,5 @@
 -- theme & transparency
 vim.o.background = "dark"
-vim.cmd.colorscheme("gruvbox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
