a246c4bce439a461b91412fd8ca41f0808ab2a17
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-10-24 23:04:20 -0300

    feat(nvim): better options and documentation

--- eggs/nvim/lua/config/opts.lua
+++ eggs/nvim/lua/config/opts.lua
@@ -5,33 +5,74 @@ vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- Basic settings
+vim.opt.number = true          -- Line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.cursorline = true      -- Highlight current line
+vim.opt.cursorlineopt = "line" -- Cursorline options
+vim.opt.wrap = false           -- Don't wrap lines
+vim.opt.scrolloff = 5          -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8      -- Keep 8 columns left/right of cursor
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- Indentation
+vim.opt.tabstop = 4        -- Tab width
+vim.opt.shiftwidth = 4     -- Indent width
+vim.opt.softtabstop = 4    -- Soft tab stop
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true  -- Copy indent from current line
+vim.opt.expandtab = true   -- Use spaces instead of tabs
 
-vim.opt.smartindent = true
+-- Search settings
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true  -- Case sensitive if uppercase in search
+vim.opt.hlsearch = false  -- Don't highlight search results
+vim.opt.incsearch = true  -- Show matches as you type
 
-vim.opt.wrap = false
+-- Visual settings
+vim.opt.termguicolors = true  -- Enable 24-bit colors
+vim.opt.signcolumn = "number" -- Always show sign column
+vim.opt.showmatch = true      -- Highlight matching brackets
+vim.opt.matchtime = 2         -- How long to show matching bracket
+vim.opt.cmdheight = 1         -- Command line height
+vim.opt.showmode = false      -- Don't show mode in command line
+vim.opt.pumheight = 10        -- Popup menu height
+vim.opt.pumblend = 10         -- Popup menu transparency
+vim.opt.winblend = 0          -- Floating window transparency
+vim.opt.conceallevel = 0      -- Don't hide markup
+vim.opt.concealcursor = ""    -- Don't hide cursor line markup
+vim.opt.lazyredraw = true     -- Don't redraw during macros
+vim.opt.synmaxcol = 500       -- Syntax highlighting limit
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("UndoDir")
-vim.opt.undofile = true
+-- File handling
+vim.opt.backup = false                            -- Don't create backup files
+vim.opt.writebackup = false                       -- Don't create backup before writing
+vim.opt.swapfile = false                          -- Don't create swap files
+vim.opt.undofile = true                           -- Persistent undo
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir") -- Undo directory
+vim.opt.timeoutlen = 500                          -- Key timeout duration
+vim.opt.ttimeoutlen = 0                           -- Key code timeout
+vim.opt.autoread = true                           -- Auto reload files changed outside vim
+vim.opt.autowrite = false                         -- Don't auto save
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Behavior settings
+vim.opt.hidden = true           -- Allow hidden buffers
+vim.opt.errorbells = false      -- No error bells
+vim.opt.autochdir = false       -- Don't auto change directory
+vim.opt.iskeyword:append("-")   -- Treat dash as part of word
+vim.opt.path:append("**")       -- include subdirectories in search
+vim.opt.selection = "exclusive" -- Selection behavior
+vim.opt.modifiable = true       -- Allow buffer modifications
 
-vim.opt.termguicolors = true
+-- Folding settings
+vim.opt.foldmethod = "expr"                             -- Use expression for folding
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folding
+vim.opt.foldlevel = 99                                  -- Start with all folds open
 
-vim.opt.updatetime = 50
+-- Split behavior
+vim.opt.splitbelow = true -- Horizontal splits go below
+vim.opt.splitright = true -- Vertical splits go right
 
-vim.have_nerd_font = true
-
-vim.cmd [[
-    autocmd BufRead * autocmd FileType <buffer> ++once
-        \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
-]]
+-- Command-line completion
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
